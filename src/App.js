7270727aa742efd3910c5642318b4e51d83cc8bb
Carlos Trujillo <carlostrujillo@Carloss-MacBook-Pro.local>
2020-03-18 18:47:27 +0100

    Added sorting Still missing styles on Created By column. Also missing filtering options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,94 @@
-import React, { Component } from 'react'
-import NavBar from './components/NavBar/NavBar'
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.scss";
+import Table from "./components/Table/Table";
 
+function App() {
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <NavBar></NavBar>
+  useEffect(() => {
+    const getQuestions = async () => {
+      try {
+        let response = await fetch("https://opentdb.com/api.php?amount=40", {
+          method: "GET"
+        });
+        let questionsData = await response.json();
+        setQuestions(questionsData.results);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getQuestions();
+    console.log(questions);
+  }, [isLoading]);
+  const randomNumGen = (min, max) => {
+    return Math.floor(Math.random() * max + min);
+  };
+
+  const randomNames = ["Karen", "Noctithra", "Coldflame", "Avalica", "Codex"];
+
+  const finalDataArray = questions.map(question => ({
+    ...question,
+    iD: randomNumGen(5000, 10000),
+    createdBy: randomNames[randomNumGen(0, randomNames.length - 1)]
+  }));
+
+  console.log(finalDataArray);
+
+  const columns = [
+    {
+      Header: "ID",
+      accessor: "iD",
+      sortType: "basic"
+    },
+    {
+      Header: "Category",
+      accessor: "category",
+      disableSortBy: true
+    },
+    {
+      Header: "Type",
+      accessor: "type",
+      disableSortBy: true
+    },
+    {
+      Header: "Difficulty",
+      accessor: "difficulty",
+      disableSortBy: true
+    },
+    {
+      Header: "Question/Statement",
+      accessor: "question",
+      disableSortBy: true
+    },
+    {
+      Header: "Created By",
+      accessor: "createdBy",
+      disableSortBy: true
+    }
+  ];
+
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <div className="title-container">
+        <h1>Browse Questions</h1>
       </div>
-    )
-  }
+      <Table
+        columns={columns}
+        data={finalDataArray}
+        defaultSorted={[
+          {
+            id: "iD",
+            desc: true
+          }
+        ]}
+        
+      ></Table>
+    </div>
+  );
 }
+
+export default App;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,11 +1,94 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
+import {
+  useTable,
+  usePagination,
+  useGroupBy,
+  useFilters,
+  useSortBy,
+  useExpanded
+} from "react-table";
+import "./Table.scss";
 
-export default class Table extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+function Table({ columns, data }) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page, 
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+      } = useTable(
+        {
+          columns,
+          data,
+          initialState: { pageIndex: 0 },
+        },
+        useSortBy,
+        usePagination
+      )
+
+  return (
+    <React.Fragment>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render("Header")}
+                {column.render("Header")==='ID'?
+            <span>
+           <i class="fa fa-sort" aria-hidden="true"></i>
+          </span>:null    
+            }
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button className='first-button' onClick={() => gotoPage(0)} >
+          {'1'}
+        </button>
+        <button onClick={() => gotoPage(1)} >
+          {'2'}
+        </button>
+        <button onClick={() => gotoPage(2)} >
+          {'3'}
+        </button>
+        <button onClick={() => gotoPage(3)} >
+          {'4'}
+        </button>
+
+        <button className='last-button' onClick={() => nextPage()}>
+          {'>'}
+        </button>
+      </div>
+      
+    </React.Fragment>
+  );
 }
+
+export default Table;
